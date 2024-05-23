c09d86fc5901ae4b5276595433dfde3cd093d3cb
Anano Makharashvili <ananomakharashvili@gmail.com>
2024-05-23 23:25:35 +0400

    coreccted

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inputEmail = document.getElementById("email-adress");
 const errorMessage = document.getElementById("error-message");
 const getSubscrubeButton = document.getElementById("subscribe-button");
+const getEmailValidate = document.getElementById("container-one-style");
+const mainContainer = document.getElementById("container");
 
 let inputText = "";
 
@@ -9,7 +11,16 @@ inputEmail.addEventListener("input", (event) => {
 });
 
 getSubscrubeButton.addEventListener("click", () => {
-  if (inputText.length > 0) {
+  if (validateEmail(inputText)) {
+    getEmailValidate.style.display = "block";
+    mainContainer.style.display = "none";
+  } else {
     errorMessage.style.display = "block";
   }
 });
+
+const validateEmail = (email) => {
+  return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+};
